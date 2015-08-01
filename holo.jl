0b80d02a74709f2f84ede7dee0a731a9a8038148
Ben Crowell <notreal@fakewillnotwork.invalid>
2015-07-31 17:10:19 -0700

    code cleanup and fixed bugs in implementation of filters

--- holo.jl
+++ holo.jl
@@ -18,19 +18,52 @@ function holo_numerator(m,k)
   end
 end
 
+# coefficients for numerical differentiation
 function holo(m,k)
   holo_numerator(m,k)/2^(2m+1)
 end
 
-#println(holo(4,1));
+# coefficients for numerical second differentiation
+# http://www.holoborodko.com/pavel/numerical-methods/numerical-derivative/smooth-low-noise-differentiators/
+# http://www.holoborodko.com/pavel/wp-content/plugins/download-monitor/download.php?id=8
+# calculate the coefficient H. notates as s(k)
+# gives wrong answer for k=0
+function holo2_numerator(k,M,N)
+  if k>M
+    0
+  else
+    if k==M then
+      1
+    else
+      ((2N-10)*holo2_numerator(k+1,M,N)-(N+2k+3)*holo2_numerator(k+2,M,N))/(N-2k-1)
+    end
+  end
+end
 
-m=20
-N=2m+3
+N=21 # is the length
+m=(N-3)/2
 M=(N-1)/2
 # println("m=",m," N=",N," M=",M)
-print("holo_",m+1," = [0,")
+println("length=",N)
+println("radius=",(N-1)/2)
+
+print("holo_",N," = [0,")
 for k in 1:M
   print(holo(m,k))
+  if k<M
+    print(",")
+  end
+end
+println("]")
+
+s0 = 0
+for k in 1:M
+  s0 = s0-2*holo2_numerator(k,M,N)
+end
+s0 = s0/2^(N-3)
+print("holo2_",N," = [",s0,",")
+for k in 1:M
+  print(holo2_numerator(k,M,N)/2^(N-3))
   if k<M
     print(",")
   end
